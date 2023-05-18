d5d9d94d1bc5b78ffe79e010edf721b02eeed76e
AntonDubovitskii <gtoll@yandex.ru>
2023-05-18 19:10:35 +0400

    Homework10 - Done

---
+++ mainapp/utils/descriptors.py
@@ -0,0 +1,18 @@
+import logging
+logger = logging.getLogger('server')
+
+
+# Дескриптор для описания порта:
+class Port:
+    def __init__(self, name):
+        self.name = name
+
+    def __set__(self, instance, value):
+        if not 1023 < value < 65536:
+            logger.critical(
+                f'Попытка запуска сервера с указанием неподходящего порта {value}. Допустимы адреса с 1024 до 65535.')
+            print(f'Попытка запуска сервера с указанием неподходящего порта {value}. Допустимы адреса с 1024 до 65535.')
+            exit(1)
+        # Если порт прошел проверку, добавляем его в список атрибутов экземпляра
+        instance.__dict__[self.name] = value
+
