7a614a15a88ed76106d4f94abf425493ec19ae35
AntonDubovitskii <96079388+AntonDubovitskii@users.noreply.github.com>
2023-05-20 13:42:21 +0400

    Homework10 - Done (#10)
    * Homework10 - Done
    
    * Homework10 - Done (metaclass name fix)

--- mainapp/client.py
+++ mainapp/client.py
@@ -1,103 +1,147 @@
 import sys
+import logging
+import time
 
 from socket import *
 from threading import Thread
-from utils.client_utils import *
 from utils.data_transfer import get_data, send_data
-from logs import client_log_config
-from errors import *
+from utils.errors import *
+from datetime import datetime
 
+from utils.metaclasses import ClientVerifier
 
 logger = logging.getLogger('client')
 
 
-def get_server_invitation(sock, username):
-    """
-    Выполняет процедуру обмена приветствиями с сервером
-    :param sock:
-    :param username:
-    :return:
-    """
-    greeting = generate_greeting(username)
-    send_data(sock, greeting)
+class Client(metaclass=ClientVerifier):
 
-    answer_dict = get_data(sock)
+    def __init__(self, username, sock):
+        self.username = username
+        self.sock = sock
 
-    if answer_dict['response'] == 200:
-        logger.info(f'Server answer to presence message: {answer_dict["response"]}')
-        return '200 OK'
-    elif answer_dict['response'] == 400:
-        logger.error(f'Server answer to presence message: {answer_dict["response"]}')
-        return '400 Bad Request'
-    else:
-        logger.error(f'Server answer to presence message: {answer_dict["response"]}')
-        return 'Произошла неизвестная ошибка'
-
-
-def read_from_socket(sock):
-
-    while True:
+    @classmethod
+    def arg_parser(cls):
         try:
-            data = get_data(sock)
-            if 'response' in data and data['response'] == 404:
-                raise UserDoesNotExist
-            print(f"{data['time']} - {data['from']} : {data['message']}")
-        except UserDoesNotExist as e:
-            print(e)
-            break
+            server_address = sys.argv[1]
+            server_port = int(sys.argv[2])
+            if server_port < 1024 or server_port > 65535:
+                raise ValueError
+        except IndexError:
+            server_address = '127.0.0.1'
+            server_port = 35000
+        except ValueError:
+            logger.error(f'Port {server_port} was entered. Available ports range from 1024 to 65535.')
+            sys.exit(1)
+        return server_address, server_port
 
+    @classmethod
+    def get_server_invitation(cls, sock, username):
+        """
+        Выполняет процедуру обмена приветствиями с сервером
+        :param sock:
+        :param username:
+        :return:
+        """
+        greeting = Client.generate_greeting(username)
+        send_data(sock, greeting)
 
-def write_to_socket(sock, name):
-    """
-    Запускается и ждет команды:
-    /pm - переход в режим личных сообщений,
-    /room - переход в режим общего чата,
-    /exit - выход
-    :param sock:
-    :param name:
-    :return:
-    """
-    while True:
-        msg = input('')
-        if msg == '/exit':
-            break
-        try:
-            if msg == '/pm':
-                to_whom = input('Введите имя того, кому хотите написать:\n')
-                while True:
-                    pm_msg = input('Сообщение: ')
-                    if pm_msg == '/exit':
-                        break
-                    dict_msg = generate_common_chat_message(pm_msg, name, to_whom)
-                    send_data(sock, dict_msg)
+        answer_dict = get_data(sock)
 
-            if msg == '/room':
-                while True:
-                    r_msg = input('')
-                    if r_msg == '/exit':
-                        break
-                    dict_msg = generate_common_chat_message(r_msg, name)
-                    send_data(sock, dict_msg)
+        if answer_dict['response'] == 200:
+            logger.info(f'Server answer to presence message: {answer_dict["response"]}')
+            return '200 OK'
+        elif answer_dict['response'] == 400:
+            logger.error(f'Server answer to presence message: {answer_dict["response"]}')
+            return '400 Bad Request'
+        else:
+            logger.error(f'Server answer to presence message: {answer_dict["response"]}')
+            return 'Произошла неизвестная ошибка'
 
-        except Exception as e:
-            print(e)
+    @classmethod
+    def generate_greeting(cls, account_name='Guest'):
+        """
+        Формирование словаря с presence-сообщением, который далее будет переведен
+        в формат json и отправлен на сервер
+        """
+        message = {
+            "action": "presence",
+            "time": time.time(),
+            "type": "status",
+            "user": {
+                "account_name": account_name,
+                "status": "I am here!"
+            }
+        }
+        return message
+
+    @classmethod
+    def generate_common_chat_message(cls, message, account_name='Guest', destination='#', current_datetime=None):
+        """
+        Формирование словаря с msg-сообщением, который далее будет переведен
+        в формат json и отправлен на сервер
+        """
+        message_dict = {
+            "action": "msg",
+            "time": datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
+            "from": account_name,
+            "to": destination,
+            "message": message
+        }
+        return message_dict
+
+    def read_from_socket(self, sock):
+
+        while True:
+            try:
+                data = get_data(sock)
+                if 'response' in data and data['response'] == 404:
+                    raise UserDoesNotExist
+                print(f"{data['time']} - {data['from']} : {data['message']}")
+            except UserDoesNotExist as e:
+                print(e)
+                break
+
+    def write_to_socket(self, sock, name):
+        """
+        Запускается и ждет команды:
+        /pm - переход в режим личных сообщений,
+        /room - переход в режим общего чата,
+        /exit - выход
+        :param sock:
+        :param name:
+        :return:
+        """
+        print('Введите одну из команд: /exit - для выхода, /pm - для перехода в режим личных сообщений, '
+              '/room - для перехода в режим общего чата')
+        while True:
+            msg = input('')
+            if msg == '/exit':
+                break
+            try:
+                if msg == '/pm':
+                    to_whom = input('Введите имя того, кому хотите написать:\n')
+                    while True:
+                        pm_msg = input('Сообщение: ')
+                        if pm_msg == '/exit':
+                            break
+                        dict_msg = Client.generate_common_chat_message(pm_msg, name, to_whom)
+                        send_data(sock, dict_msg)
+
+                if msg == '/room':
+                    while True:
+                        r_msg = input('')
+                        if r_msg == '/exit':
+                            break
+                        dict_msg = Client.generate_common_chat_message(r_msg, name)
+                        send_data(sock, dict_msg)
+
+            except Exception as e:
+                print(e)
 
 
 def main():
 
-    try:
-        server_address = sys.argv[1]
-        server_port = int(sys.argv[2])
-        if server_port < 1024 or server_port > 65535:
-            raise ValueError
-    except IndexError:
-        server_address = '127.0.0.1'
-        server_port = 35000
-    except ValueError:
-        logger.error(f'Port {server_port} was entered. Available ports range from 1024 to 65535.')
-        sys.exit(1)
-
-    address = (server_address, server_port)
+    address = (Client.arg_parser())
     user_name = input('Введите ваше имя:\n')
 
     transport_socket = socket(AF_INET, SOCK_STREAM)
@@ -105,13 +149,15 @@ def main():
 
     # Создание потоков начинается после того, как сервер одобрил подключение, пока он всегда одобряет правильный запрос,
     # но потом возможно будет аутентификация или система банов
-    server_answer = get_server_invitation(transport_socket, user_name)
+    server_answer = Client.get_server_invitation(transport_socket, user_name)
     if server_answer == '200 OK':
-        read_thread = Thread(target=read_from_socket, args=(transport_socket, ))
+        client_obj = Client(user_name, transport_socket)
+
+        read_thread = Thread(target=client_obj.read_from_socket, args=(client_obj.sock, ))
         read_thread.daemon = True
         read_thread.start()
 
-        write_thread = Thread(target=write_to_socket, args=(transport_socket, user_name))
+        write_thread = Thread(target=client_obj.write_to_socket, args=(client_obj.sock, client_obj.username))
         write_thread.daemon = True
         write_thread.start()
     else:
--- mainapp/server.py
+++ mainapp/server.py
@@ -1,126 +1,197 @@
 import select
 import logging
 import sys
+import time
+import socket
 
-from socket import socket, AF_INET, SOCK_STREAM
 from utils.data_transfer import get_data, send_data
-from utils.server_utils import *
 from logs import server_log_config
+from utils.descriptors import Port
+from utils.metaclasses import ServerVerifier
 
 logger = logging.getLogger('server')
 
 
-def read_requests(r_clients, all_clients):
-    """
-    Формирует словарь, содержащий объекты сокетов в качестве ключа и словарь с переданными данными
-    в качестве значения
-    :param r_clients:
-    :param all_clients:
-    :return:
-    """
-    responses = {}
+def arg_parser():
+    if '-a' in sys.argv:
+        listen_address = sys.argv[sys.argv.index('-a') + 1]
+    else:
+        listen_address = ''
 
-    for sock in r_clients:
-        try:
-            data = get_data(sock)
-            responses[sock] = data
-        except:
-            sock.close()
-            all_clients.remove(sock)
+    if '-p' in sys.argv:
 
-    return responses
+        listen_port = int(sys.argv[sys.argv.index('-p') + 1])
+
+    else:
+        listen_port = 7777
+    return listen_address, listen_port
 
 
-def write_responses(requests, w_clients, all_clients, clients_registered):
-    """
-    Проход по словарю с полученными данными выполнение дейтвий соответствующих типу полученного сообщения
-    :param requests:
-    :param w_clients:
-    :param all_clients:
-    :param clients_registered:
-    :return:
-    """
+class Server(metaclass=ServerVerifier):
 
-    for message_sock, message in requests.items():
+    port = Port("port")
+
+    def __init__(self, listen_ipaddress):
+        self.addr = listen_ipaddress
+        self.clients_temp = []
+        # Словарь, временно имитирующий бд, где регистрируются пользователи
+        self.clients_registered = {}
+
+    @classmethod
+    def generate_presence_answer(cls, data: dict):
+        if not isinstance(data, dict):
+            raise ValueError
+        msg = {
+            "response": 200,
+            "time": time.time(),
+            "alert": f"Привет {data['user']['account_name']}!"
+        }
+        return msg
+
+    @classmethod
+    def generate_no_user_error_msg(cls, data: dict):
+        if not isinstance(data, dict):
+            raise ValueError
+        msg = {
+            "response": 404,
+            "time": time.time(),
+            "error": f"Ошибка 404! Пользователь {data['to']} не зарегистрирован"
+        }
+        return msg
+
+    @classmethod
+    def generate_user_not_online_error_msg(cls, data: dict):
+        if not isinstance(data, dict):
+            raise ValueError
+        msg = {
+            "response": 410,
+            "time": time.time(),
+            "error": f"Ошибка 410! Пользователь {data['to']} не в сети"
+        }
+        return msg
+
+    @classmethod
+    def identify_msg_type(cls, data: dict):
         """
-        Проверка типа сообщения, в зависимости от этого сервер посылает сообщение дальше, всему чату, в лс, либо
-        генерирует ответ на приветствие
+        Проверка типа пришедшего сообщения
+        :param data:
+        :return:
         """
-        if identify_msg_type(message) == 'presense_msg':
-            clients_registered[message['user']['account_name']] = message_sock
-            send_data(message_sock, generate_presence_answer(message))
-            continue
+        if not isinstance(data, dict):
+            raise ValueError
 
-        elif identify_msg_type(message) == 'p2p_chat_msg':
+        if 'action' in data and 'time' in data and 'user' in data \
+                and data['action'] == 'presence' and data['user']['account_name']:
+            msg_type = 'presense_msg'
+        elif 'action' in data and 'time' in data and 'from' in data and 'to' in data and 'message' in data \
+                and data['action'] == 'msg' and data['to'] == '#':
+            msg_type = 'common_chat_msg'
+        elif 'action' in data and 'time' in data and 'from' in data and 'to' in data and 'message' in data \
+                and data['action'] == 'msg' and data['to'] != '#':
+            msg_type = 'p2p_chat_msg'
+        else:
+            msg_type = 'incorrect message'
+
+        return msg_type
+
+    def read_requests(self, r_clients, all_clients):
+        """
+        Формирует словарь, содержащий объекты сокетов в качестве ключа и словарь с переданными данными
+        в качестве значения
+        :param r_clients:
+        :param all_clients:
+        :return:
+        """
+        responses = {}
+
+        for sock in r_clients:
             try:
-                if message['to'] in clients_registered:
-                    send_data(clients_registered[message['to']], message)
-                    send_data(message_sock, message)
-                else:
-                    send_data(message_sock, generate_no_user_error_msg(message))
+                data = get_data(sock)
+                responses[sock] = data
             except:
-                message_sock.close()
-                all_clients.remove(message_sock)
+                sock.close()
+                all_clients.remove(sock)
 
-        elif identify_msg_type(message) == 'common_chat_msg':
-            for sock in w_clients:
+        return responses
+
+    def write_responses(self, requests, w_clients, all_clients, clients_registered):
+        """
+        Проход по словарю с полученными данными выполнение дейтвий соответствующих типу полученного сообщения
+        :param requests:
+        :param w_clients:
+        :param all_clients:
+        :param clients_registered:
+        :return:
+        """
+
+        for message_sock, message in requests.items():
+            """
+            Проверка типа сообщения, в зависимости от этого сервер посылает сообщение дальше, всему чату, в лс, либо
+            генерирует ответ на приветствие
+            """
+            if Server.identify_msg_type(message) == 'presense_msg':
+                clients_registered[message['user']['account_name']] = message_sock
+                send_data(message_sock, Server.generate_presence_answer(message))
+                continue
+
+            elif Server.identify_msg_type(message) == 'p2p_chat_msg':
                 try:
-                    send_data(sock, message)
+                    if message['to'] in clients_registered:
+                        send_data(clients_registered[message['to']], message)
+                        send_data(message_sock, message)
+                    else:
+                        send_data(message_sock, Server.generate_no_user_error_msg(message))
                 except:
-                    sock.close()
-                    all_clients.remove(sock)
+                    message_sock.close()
+                    all_clients.remove(message_sock)
+
+            elif Server.identify_msg_type(message) == 'common_chat_msg':
+                for sock in w_clients:
+                    try:
+                        send_data(sock, message)
+                    except:
+                        sock.close()
+                        all_clients.remove(sock)
+
+    def init_socket(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((self.addr, self.port))
+        s.settimeout(0.2)
+
+        self.sock = s
+        self.sock.listen()
+
+    def main_loop(self):
+        self.init_socket()
+        while True:
+            try:
+                conn, addr = self.sock.accept()
+
+            except OSError as e:
+                pass
+            else:
+                logger.debug(f"Получен запрос на соединение от {str(addr)}")
+                self.clients_temp.append(conn)
+            finally:
+                wait = 1
+                to_receive_list = []
+                to_send_list = []
+                try:
+                    to_receive_list, to_send_list, e = select.select(self.clients_temp, self.clients_temp, [], wait)
+                except:
+                    pass
+
+                requests = self.read_requests(to_receive_list, self.clients_temp)
+                if requests:
+                    self.write_responses(requests, to_send_list, self.clients_temp, self.clients_registered)
 
 
 def main():
 
-    try:
-        if '-a' in sys.argv:
-            listen_address = sys.argv[sys.argv.index('-a') + 1]
-        else:
-            listen_address = ''
-
-        if '-p' in sys.argv:
-            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
-        else:
-            listen_port = 35000
-
-        if listen_port < 1024 or listen_port > 65535:
-            raise ValueError
-    except ValueError:
-        logger.error(f'Port {listen_port} was entered. Available ports range from 1024 to 65535')
-        sys.exit(1)
-
-    address = (listen_address, listen_port)
-    clients_temp = []
-    # Словарь, временно имитирующий бд, где регистрируются пользователи
-    clients_registered = {}
-
-    s = socket(AF_INET, SOCK_STREAM)
-    s.bind(address)
-    s.listen(5)
-    s.settimeout(0.2)
-
-    while True:
-        try:
-            conn, addr = s.accept()
-
-        except OSError as e:
-            pass
-        else:
-            logger.debug(f"Получен запрос на соединение от {str(addr)}")
-            clients_temp.append(conn)
-        finally:
-            wait = 1
-            to_receive_list = []
-            to_send_list = []
-            try:
-                to_receive_list, to_send_list, e = select.select(clients_temp, clients_temp, [], wait)
-            except:
-                pass
-
-            requests = read_requests(to_receive_list, clients_temp)
-            if requests:
-                write_responses(requests, to_send_list, clients_temp, clients_registered)
+    listen_ipaddress, listen_port = arg_parser()
+    server = Server(listen_ipaddress)
+    server.port = listen_port
+    server.main_loop()
 
 
 if __name__ == '__main__':
@@ -129,5 +200,6 @@ if __name__ == '__main__':
         logger.info('Сервер запущен')
         main()
     except Exception as e:
+        print(e)
         logger.critical(e)
 
